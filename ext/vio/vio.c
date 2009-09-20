cf68c502b6ecc50eeef7be73e1de59a1604c54ec
Lourens Naudé <lourens@methodmissing.com>
2009-09-20 02:18:57 +0100

    Implement IO.readv

--- ext/vio/vio.c
+++ ext/vio/vio.c
@@ -1,4 +1,7 @@
 #include "ruby.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 #ifndef RSTRING_PTR
 #define RSTRING_PTR(obj) RSTRING(obj)->ptr
@@ -28,11 +31,52 @@
 static VALUE 
 vio_read(VALUE io, VALUE iov)
 {
+    Check_Type(iov, T_ARRAY);
+    int fd;
+#ifdef RUBY19
+    rb_io_t *fptr;
+#else
+    OpenFile *fptr;
+#endif
+    if (RARRAY_LEN(iov) == 0) rb_raise(rb_eIOError, "No buffer offsets given");  
+    GetOpenFile(io, fptr);
+    rb_io_check_readable(fptr);
+#ifdef RUBY19
+     fd = fptr->fd;
+#else
+     fd = fileno(fptr->f);
+#endif
+    struct iovec iovs[IOV_MAX];
+    int i, size, bytes_read;
+    int expected = 0;
+    int cnt = RARRAY_LEN(iov);
+    VALUE results = rb_ary_new2(cnt);
+    for (i=0; i < cnt; i++) {
+      size = FIX2INT(RARRAY_PTR(iov)[i]);
+      expected = expected + size;
+      iovs[i].iov_len = size;
+      iovs[i].iov_base = malloc(size);
+    }    
+    bytes_read = readv(fd,iovs,cnt);
+    if (bytes_read < expected) rb_raise(rb_eIOError, "Vectored I/O read failure!");
+    for (i=0; i < cnt; i++) {
+      rb_ary_push(results, rb_tainted_str_new((char *)iovs[i].iov_base, iovs[i].iov_len));
+    }
+    return results;
 }
 
 static VALUE 
 vio_write(VALUE io, VALUE iov)
 {
+   Check_Type(iov, T_ARRAY);
+#ifdef RUBY19
+    rb_io_t *fptr;
+#else
+    OpenFile *fptr;
+#endif
+    GetOpenFile(io, fptr);
+    rb_io_check_writable(fptr);
+    return Qnil;
 }
 
 void Init_vio()
--- test/test_vio.rb
+++ test/test_vio.rb
@@ -3,11 +3,32 @@ require File.dirname(__FILE__) + '/helper'
 
 class TestVectoredIO < Test::Unit::TestCase
   
-  def test_defines_readv_and_writev
-    io = File.open(fixture('db.txt'))
-    assert io.respond_to? :readv
-    assert io.respond_to? :writev
+  def test_readv_arguments
+    io = File.open(fixture('fix.txt'))
+    assert_raises TypeError do
+      io.readv(nil)
+    end
+    assert_raises IOError do    
+      io.readv []
+    end
+    io.readv [1]
     io.close
   end
   
+  def test_readv
+    io = File.open(fixture('fix.txt'))
+    expected = [ "8=FIX.4.4", "\x019=45", "\x0135=0", "\x0149=TW", "\x0156=ISLD",
+"\x0134=3\x0152=20000426-12:05:06", "\x0110=220" ]
+    assert_equal expected, io.readv([9, 5, 5, 6, 8, 26, 7])
+    io.close
+  end  
+   
+  def test_writev_arguments
+    io = File.open(fixture('writable.txt'), 'w')
+    assert_raises TypeError do
+      io.writev(nil)
+    end
+    io.writev []
+    io.close
+  end  
 end
